d84a368f57b75dba568917840a0575fd737db061
Shreya-sinha <sinhashreya2679@gmail.com>
2023-09-26 17:47:55 +0530

    updated App.js

--- astro-gallery/src/App.js
+++ astro-gallery/src/App.js
@@ -1,25 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <nav className="navbar bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Navbar
-        </a>
-        <form className="d-flex" role="search">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success" type="submit">
-            Search
-          </button>
-        </form>
-      </div>
-    </nav>
+    <>
+      <Navbar title="Image-Gallery" />
+    </>
   );
 }
 
