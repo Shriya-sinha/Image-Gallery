a2d08e6d62298bd27026b7454155c65f8203a2eb
Shreya-sinha <sinhashreya2679@gmail.com>
2023-10-01 12:45:24 +0530

    updated uploadform.js

--- astro-gallery/src/components/uploadform.js
+++ astro-gallery/src/components/uploadform.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-const uploadform = () => {
+const Uploadform = () => {
+  const [imag, setImag] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ["image/png", "image/jpeg"];
+
+  const changeHandler = (e) => {
+    let selected = e.target.files[0];
+
+    if (selected && types.includes(selected.types)) {
+      setImag(selected);
+    } else {
+      setImag(null);
+      setError("Please select an image file of type png or jprg");
+    }
+  };
   return (
     <form>
-      <input type="file" />
+      <input type="file" onChange={changeHandler} />
     </form>
   );
 };
 
-export default uploadform;
+export default Uploadform;
