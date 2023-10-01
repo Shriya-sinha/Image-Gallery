5ad6b310e85c4af909fe205ec5cf36a01e3d3f7d
Shreya-sinha <sinhashreya2679@gmail.com>
2023-10-01 12:20:08 +0530

    updated App.js

--- astro-gallery/src/App.js
+++ astro-gallery/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import uploadform from "./components/uploadform";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Navbar title="Image-Gallery" />
-    </>
+      <uploadform />
+    </div>
   );
 }
 
