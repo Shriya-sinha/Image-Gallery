f6af3d43fef4f02e1deb52b1bb136a775145cc0f
Shreya-sinha <sinhashreya2679@gmail.com>
2023-10-01 13:42:38 +0530

    updated uploadform.js

--- astro-gallery/src/components/uploadform.js
+++ astro-gallery/src/components/uploadform.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 const Uploadform = () => {
-  const [imag, setImag] = useState(null);
+  const [imag, setimag] = useState(null);
   const [error, setError] = useState(null);
 
   const types = ["image/png", "image/jpeg"];
@@ -9,16 +10,22 @@ const Uploadform = () => {
   const changeHandler = (e) => {
     let selected = e.target.files[0];
 
-    if (selected && types.includes(selected.types)) {
-      setImag(selected);
+    if (selected && types.includes(selected.type)) {
+      setimag(selected);
+      setError(null);
     } else {
-      setImag(null);
-      setError("Please select an image file of type png or jprg");
+      setimag(null);
+      setError("Please select an image file of type png or jpeg");
     }
   };
   return (
     <form>
       <input type="file" onChange={changeHandler} />
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+        {imag && <div>{imag.name}</div>}
+        {imag && <ProgressBar imag={imag} setimag={setimag} />}
+      </div>
     </form>
   );
 };
