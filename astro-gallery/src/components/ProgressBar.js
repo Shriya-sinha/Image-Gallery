a33574d503f07585731b387afdba9291bbb61007
Shreya-sinha <sinhashreya2679@gmail.com>
2023-10-02 14:31:00 +0530

    update ProgressBar.js

--- astro-gallery/src/components/ProgressBar.js
+++ astro-gallery/src/components/ProgressBar.js
@@ -1,8 +1,13 @@
 import React from "react";
 import useStorage from "../hooks/useStorage";
 
-const ProgressBar = () => {
-  return <div className="progress-bar">progress</div>;
+const ProgressBar = ({ imag, setImag }) => {
+  const { url, progress } = useStorage(imag);
+  return (
+    <div className="progress-bar" style={{ width: progress + "%" }}>
+      progress
+    </div>
+  );
 };
 
 export default ProgressBar;
