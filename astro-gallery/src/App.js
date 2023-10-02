432a470264822379aebe052c5ae415c6adb5ba82
Shreya-sinha <sinhashreya2679@gmail.com>
2023-10-02 14:30:40 +0530

    update App.js

--- astro-gallery/src/App.js
+++ astro-gallery/src/App.js
@@ -1,8 +1,20 @@
+import { getDatabase, ref, set } from "firebase/database";
+import { app } from "./firebase/config";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Uploadform from "./components/uploadform";
+import setStorage from "";
+
+const db = getDatabase(app);
 
 function App() {
+  const outdata = () => {
+    set(ref(db, "users/shrey"), {
+      id: 1,
+      name: "Shreya",
+      age: 20,
+    });
+  };
   return (
     <div className="App">
       <Navbar title="Image-Gallery" />
