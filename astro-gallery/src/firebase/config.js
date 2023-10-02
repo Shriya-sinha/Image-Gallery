99961d003e1b37ddce4e47d74a94c41fd277cf5f
Shreya-sinha <sinhashreya2679@gmail.com>
2023-10-02 14:31:14 +0530

    update config.js

--- astro-gallery/src/firebase/config.js
+++ astro-gallery/src/firebase/config.js
@@ -2,21 +2,21 @@
 import * as firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-// Your web app's Firebase configuration
+
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDqLCxeC_w-4k4H2-frC6vnNhQV8StQe8w",
-  authDomain: "image-gallery-2e7cc.firebaseapp.com",
-  projectId: "image-gallery-2e7cc",
-  storageBucket: "image-gallery-2e7cc.appspot.com",
-  messagingSenderId: "88083642508",
-  appId: "1:88083642508:web:d28545054a15c77a9523a9",
+  apiKey: "AIzaSyBcuDtqiBO2IsKfyVFKLs0hiqOIUA1wna8",
+  authDomain: "astro-image-gallery.firebaseapp.com",
+  projectId: "astro-image-gallery",
+  storageBucket: "astro-image-gallery.appspot.com",
+  messagingSenderId: "26270280606",
+  appId: "1:26270280606:web:3879d2efe1a0c99b4d848f",
+  dataBaseURL: "https://astro-image-gallery-default-rtdb.firebaseio.com/",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
 
-export { projectStorage, projectFirestore };
+export { app, projectStorage, projectFirestore };
